a9f9a697e3260ef01d4eafde86f4f067bbe02df0
csw <csw@aa.com>
2017-09-25 01:56:58 +0300

    test

--- EarthquakeActivity.java
+++ EarthquakeActivity.java
@@ -40,6 +40,7 @@ public class EarthquakeActivity extends AppCompatActivity {
         earthquakes.add("Moscow");
         earthquakes.add("Rio de Janeiro");
         earthquakes.add("Paris");
+        earthquakes.add("Paris");
 
 
         // Find a reference to the {@link ListView} in the layout
